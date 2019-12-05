adbb5eb7370a6e4e097e47ba5326cb1e14eaa2bc
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-12-05 01:39:49 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,5 +1,8 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+<<<<<<< HEAD
 
 class Costume < ActiveRecord::Base
 end
+=======
+>>>>>>> 7ffcc4b755a5c23c6432f764372fb8512c8138c1
